f3f7c5dc665ba9d2b1e0d1ce7b7727bb8fdc7906
Antonio Terreno <antonio.terreno@gmail.com>
2020-12-01 12:26:56 +0000

    some cleanup/modern es6 syntax and only circleci

--- docs/backends.js
+++ docs/backends.js
@@ -1,46 +1,16 @@
-/*
-Build type:
-{
-   repository: name,
-   branch: 'master',
-   started: new Date(),
-   state: 'success',
-   commit: {
-      created: new Date(),
-      author: 'Tester',
-      hash: 'shahash'
-   }
-}
-*/
-
 function buildBackend(settings, callback) {
-  var backend = circleBackend
-  if (settings.mode === 'travis') {
-    backend = travisBackend
-  } else if (settings.mode === 'jenkins') {
-    backend = jenkinsBackend
-  }
-  var branchFilter = function(build) {
-    return settings.branch ? build.branch.match(settings.branch) : true
-  }
-  var repositoryFilter = function(build) {
-    return settings.repositories
-      ? settings.repositories.split(',').includes(build.repository)
-      : true
-  }
+  const branchFilter = (build) => (settings.branch ? build.branch.match(settings.branch) : true)
+  const repositoryFilter = (build) =>
+    settings.repositories ? settings.repositories.split(',').includes(build.repository) : true
 
-  return function() {
-    backend(settings, function(err, data) {
+  return () => {
+    circleBackend(settings, (err, data) => {
       if (err) {
         return callback(err)
       }
       let builds = data.filter(repositoryFilter).filter(branchFilter)
-      builds = _.uniqBy(builds, function(b) {
-        return b.repository + b.branch
-      })
-      builds = builds.sort(function(a, b) {
-        return a.started.getTime() - b.started.getTime()
-      })
+      builds = _.uniqBy(builds, (b) => b.repository + b.branch)
+      builds = builds.sort((a, b) => a.started.getTime() - b.started.getTime())
       callback(undefined, builds)
     })
   }
@@ -50,265 +20,90 @@ function backendOptions() {
   return {
     circle: {
       name: 'Circle CI',
-      url: 'https://circleci.com/api/v1/projects',
-      token: undefined
+      url: 'https://circleci.com/api/v1.1/projects',
+      token: process.env.CIRCLE_CI_TOKEN,
     },
-    travis: {
-      name: 'Travis CI',
-      url: 'https://api.travis-ci.com/repos',
-      token: undefined
-    },
-    jenkins: {
-      name: 'Jenkins CI',
-      url: undefined,
-      token: undefined
-    }
   }
 }
 
-function httpRequest(url, handler /*, headers */) {
-  var request = new XMLHttpRequest()
-  var headers = arguments[2] || {}
-  request.open('GET', url, true)
-  Object.keys(headers).forEach(function(headerName) {
-    request.setRequestHeader(headerName, headers[headerName])
-  })
-  request.onload = function() {
-    if (request.status === 401 || request.status === 403) {
-      handler('Invalid API token (' + request.status + ' ' + request.responseText + ')')
-    } else if (request.status >= 200 && request.status < 400) {
-      try {
-        var data = JSON.parse(request.responseText)
-        handler(undefined, data)
-      } catch (exc) {
-        console.log('Error fetching URL', url, request.responseText)
-        handler(exc)
-      }
-    } else {
-      handler('Error getting URL ' + url + ':' + request.status)
-    }
-  }
-  request.send()
-}
-
-var travisBackend = function(settings, resultCallback) {
-  var travisRequest = function(url, cb) {
-    var handler = function(err, data) {
-      if (err) {
-        resultCallback(err)
-      } else {
-        cb(data)
-      }
-    }
-    httpRequest(url, handler, {
-      Accept: 'application/vnd.travis-ci.2+json',
-      Authorization: 'token ' + settings.token
+async function httpRequest(url) {
+  try {
+    const response = await fetch('https://cors-anywhere.herokuapp.com/' + url, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
     })
-  }
-
-  var translateBuild = function(reponame, commits) {
-    var findCommit = function(commit) {
-      return commits.find(function(c) {
-        return c.id == commit
-      })
+    if (response.status === 401 || response.status === 403) {
+      console.log('Invalid API token (' + response.status + ' ' + response.responseText + ')')
+      return {}
     }
-    return function(b) {
-      var commit = findCommit(b.commit_id)
-      return {
-        repository: reponame,
-        branch: commit.branch,
-        started: new Date(b.started_at),
-        state: b.state,
-        commit: {
-          created: new Date(commit.committed_at),
-          author: commit.author_name,
-          hash: commit.sha
-        }
-      }
+    if (response.status >= 200 && response.status < 400) {
+      return response.json()
     }
+  } catch (error) {
+    console.log('Error fetching URL', url)
+    console.log(error)
   }
-
-  var parseBuilds = function(repos) {
-    var responses = []
-    repos.forEach(function(r) {
-      travisRequest(settings.url + '/' + r.name + '/builds', function(data) {
-        var reponame = r.name.split('/')[1]
-        var builds = data.builds.map(translateBuild(reponame, data.commits))
-        responses.push(builds)
-        if (responses.length === repos.length) {
-          var result = responses.reduce(function(acc, item) {
-            return item.length > 0 ? acc.concat(item) : acc
-          }, [])
-          resultCallback(undefined, result)
-        }
-      })
-    })
-  }
-
-  travisRequest(settings.url, function(data) {
-    parseBuilds(
-      data.repos.map(function(repo) {
-        return { id: repo.id, name: repo.slug }
-      })
-    )
-  })
 }
 
-var circleBackend = function(settings, resultCallback) {
-  var url = settings.url + '?circle-token=' + settings.token
+const circleBackend = (settings, resultCallback) => {
+  const url = `${settings.url}?circle-token=${settings.token}`
 
-  httpRequest(
-    url,
-    function(err, data) {
-      if (err) {
-        return resultCallback(err)
-      }
-      var builds = data.reduce(function(acc, repository) {
-        return acc.concat(
-          Object.keys(repository.branches).map(function(branchName) {
-            var branch = repository.branches[branchName]
-            var hasNeverBuilt = !branch.running_builds && !branch.recent_builds
-            if (hasNeverBuilt) {
-              console.warn('Repository', repository.reponame, 'has a branch named', branchName, 'that has never been built')
-              return
-            }
-            var buildIsRunning = branch.running_builds.length != 0
-            var build = buildIsRunning ? branch.running_builds[0] : branch.recent_builds[0]
-            var status = buildIsRunning ? build.status : build.outcome
-            return {
-              repository: repository.reponame,
-              branch: branchName,
-              started: new Date(build.pushed_at),
-              state: status,
-              commit: {
-                created: new Date(build.pushed_at),
-                author: null,
-                hash: build.vcs_revision
+  function findLatestCreatedAtTime(workflowValues) {
+    return workflowValues.map((x) => new Date(x.created_at).getTime()).sort()[
+      workflowValues.length - 1
+    ]
+  }
+
+  function currentWorkflowsStatus(workflowValues, latestTime) {
+    const currentWorkflowStatuses = workflowValues
+      .filter((x) => new Date(x.created_at).getTime() === latestTime)
+      .map((x) => x.status)
+    if (currentWorkflowStatuses.includes('failed')) {
+      return 'failed'
+    }
+    return currentWorkflowStatuses[0]
+  }
+
+  const parse = (data) => {
+    return data
+      .reduce(
+        (acc, repository) =>
+          acc.concat(
+            Object.keys(repository.branches).map((branchName) => {
+              const branch = repository.branches[branchName]
+              const hasNeverBuilt = !branch.running_builds && !branch.recent_builds
+              if (hasNeverBuilt) {
+                return
               }
-            }
-          })
-        )
-      }, []).filter(function (build) { return !!build })
-      resultCallback(undefined, builds)
-    },
-    {
-      Accept: 'application/json'
-    }
-  )
-}
-
-var jenkinsBackend = function(settings, resultCallback) {
-  var jenkinsRequest = function(url, cb) {
-    var handler = function(err, data) {
-      if (err) {
-        resultCallback(err)
-      } else {
-        cb(data)
-      }
-    }
-    var headers = {}
-    if (settings.token) {
-      headers.Authorization = 'Basic ' + window.btoa(settings.token)
-    }
-    httpRequest(url, handler, headers)
-  }
-
-  var findLastCommit = function(builds) {
-    if (!builds) {
-      return undefined
-    }
-    var lastBuildWithCommits = builds.filter(function(b) {
-      return b.changeSets && b.changeSets.length > 0
-    })[0]
-    if (!lastBuildWithCommits) {
-      return undefined
-    }
-    var lastCommits = lastBuildWithCommits.changeSets[0].items.map(function(item) {
-      return {
-        created: new Date(item.timestamp),
-        author: item.author.fullName,
-        hash: item.commitId
-      }
-    })
-    return lastCommits[lastCommits.length - 1]
-  }
-
-  var findBuildReason = function(builds) {
-    if (!builds) {
-      return undefined
-    }
-    var lastBuildWithActions = builds.filter(function(b) {
-      return b.actions && b.actions.length > 0
-    })[0]
-    if (!lastBuildWithActions) {
-      return undefined
-    }
-    var reason = lastBuildWithActions.actions
-      .filter(function(a) {
-        return a._class == 'hudson.model.CauseAction'
-      })
-      .reduce(function(acc, a) {
-        return acc.concat(
-          a.causes.map(function(c) {
-            return c.shortDescription
-          })
-        )
-      }, [])
-      .join(';')
-
-    return {
-      author: reason
-    }
-  }
-
-  var findResponsible = function(job) {
-    if (!job.actions) {
-      return undefined
-    }
-    var contributorActions = job.actions.filter(function(action) {
-      return action.contributor || action.contributorDisplayName
-    })
-    var contributor = contributorActions[0]
-    if (!contributor) {
-      return undefined
-    }
-    return {
-      created: new Date(job.timestamp),
-      author: contributor.contributorDisplayName || contributor.contributor
-    }
-  }
-
-  var url =
-    settings.url +
-    '/api/json?depth=4&tree=name,url,jobs[name,url,jobs[name,url,actions[contributor,contributorDisplayName,contributorEmail],buildable,builds[result,building,actions[causes[shortDescription]],changeSets[items[author[fullName],timestamp,commitId]],timestamp]]]'
-  jenkinsRequest(url, function(data) {
-    var builds = data.jobs.reduce(function(acc, project) {
-      return acc.concat(
-        project.jobs.reduce(function(acc, job) {
-          if (!job.buildable) {
-            return acc
-          }
-          var build = job.builds[0] || {}
-          var result = 'failed'
-          if (build.building) {
-            result = 'started'
-          } else if (build.result === 'ABORTED') {
-            result = 'canceled'
-          } else if (build.result === 'SUCCESS') {
-            result = 'success'
-          }
-
-          return acc.concat({
-            repository: project.name,
-            branch: job.name,
-            started: new Date(build.timestamp),
-            state: result,
-            commit:
-              findLastCommit(job.builds) || findResponsible(job) || findBuildReason(job.builds)
-          })
-        }, [])
+              const buildIsRunning = branch.running_builds.length != 0
+              const build = buildIsRunning ? branch.running_builds[0] : branch.recent_builds[0]
+              let status = buildIsRunning ? build.status : build.outcome
+              if (branch.is_using_workflows) {
+                const workflowValues = Object.values(branch.latest_workflows)
+                latestCreatedAtTime = findLatestCreatedAtTime(workflowValues)
+                status = currentWorkflowsStatus(workflowValues, latestCreatedAtTime)
+              }
+              return {
+                repository: repository.reponame,
+                branch: branchName,
+                started: new Date(build.pushed_at),
+                state: status,
+                commit: {
+                  created: new Date(build.pushed_at),
+                  author: null,
+                  hash: build.vcs_revision,
+                },
+              }
+            })
+          ),
+        []
       )
-    }, [])
+      .filter((build) => !!build)
+  }
+
+  ;(async () => {
+    const data = await httpRequest(url)
+    const builds = parse(data)
     resultCallback(undefined, builds)
-  })
+  })()
 }
